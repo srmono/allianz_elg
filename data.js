7884da8a7c29d397bfcde881377e53ed931f7e44
srmono <bvsrao91@gmail.com>
2022-11-14 16:32:06 +0530

    ES initial commit

---
+++ data.js
@@ -0,0 +1,22 @@
+//JavaScript Object Notation
+
+//String, Number, Array, Object 
+
+var person = {
+    name: "Venkat",
+    exp: 18,
+    skills: ["Frontend", "Backend"],
+    address: {
+        city: "bangalore",
+        geo: {
+            lat: "",
+            lan:""
+        }
+    }
+}
+
+//js object to json (stringified)
+
+JSON.stringify(person)
+
+//DATA  -->  DSL -> JSON
